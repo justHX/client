a7fcc4fc2021645cfd5438595a1b89b9dba1f758
d.erohin <d.erohin@zebrains.team>
2022-06-15 18:55:08 +0300

    fix routing

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,14 +1,14 @@
-import {FC} from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
+import { FC } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import {SecurePage} from "components";
+import { SecurePage } from "components";
 import VolonteerPage from "pages/persons/VolonteerPage";
 import Auth from "pages/auth/Auth";
 import Welcome from "pages/Welcome";
 
-import {ROUTES} from "const/Routes";
+import { ROUTES } from "const/Routes";
 
-import {UserRole, useUser} from "stores";
+import { UserRole, useUser } from "stores";
 import UsersAdmin from "./pages/admin/UsersAdmin";
 import VolonteerAdmin from "./pages/admin/VolonteerAdmin";
 import FeedbackAdmin from "./pages/admin/FeedbackAdmin";
@@ -18,124 +18,109 @@ import ClaimUser from "./pages/impoverished/ClaimUser";
 import EditUser from "./pages/impoverished/EditUser";
 import StoryUser from "./pages/impoverished/StoryUser";
 
-const Routing: FC = () => {
+interface ProtectedPageProps {
+  role: UserRole;
+  children: JSX.Element;
+}
+
+const ProtectedPage: FC<ProtectedPageProps> = ({ children, role }) => {
   const { user } = useUser();
 
+  const isAuth = user.isAuth && user.role === role;
+
+  return (
+    <SecurePage isAuth={isAuth} redirect={ROUTES.LOGIN_ROUTE}>
+      {children}
+    </SecurePage>
+  );
+};
+
+const Routing: FC = () => {
   return (
     <Routes>
       <Route
         path={ROUTES.ADMIN_ROUTE}
         element={
-          <SecurePage
-            isAuth={user.isAuth && user.role === UserRole.ADMIN}
-            redirect={ROUTES.LOGIN_ROUTE}
-          >
-              <Navigate to={ROUTES.ADMIN_ROUTE_USER} />
-          </SecurePage>
+          <ProtectedPage role={UserRole.ADMIN}>
+            <Navigate to={ROUTES.ADMIN_ROUTE_USER} />
+          </ProtectedPage>
         }
       />
-        <Route
-            path={ROUTES.ADMIN_ROUTE_USER}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <UsersAdmin />
-                </SecurePage>
-            }
-        />
-        <Route
-            path={ROUTES.ADMIN_ROUTE_VOLONTEER}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <VolonteerAdmin />
-                </SecurePage>
-            }
-        />
-        <Route
-            path={ROUTES.ADMIN_ROUTE_FEEDBACK}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <FeedbackAdmin />
-                </SecurePage>
-            }
-        />
-        <Route
-            path={ROUTES.ADMIN_ROUTE_TELEGRAMM}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <TelegramAdmin />
-                </SecurePage>
-            }
-        />
-        <Route
-            path={ROUTES.ADMIN_ROUTE_SETTINGS}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <SettingsAdmins />
-                </SecurePage>
-            }
-        />
+      <Route
+        path={ROUTES.ADMIN_ROUTE_USER}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <UsersAdmin />
+          </ProtectedPage>
+        }
+      />
+      <Route
+        path={ROUTES.ADMIN_ROUTE_VOLONTEER}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <VolonteerAdmin />
+          </ProtectedPage>
+        }
+      />
+      <Route
+        path={ROUTES.ADMIN_ROUTE_FEEDBACK}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <FeedbackAdmin />
+          </ProtectedPage>
+        }
+      />
+      <Route
+        path={ROUTES.ADMIN_ROUTE_TELEGRAMM}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <TelegramAdmin />
+          </ProtectedPage>
+        }
+      />
+      <Route
+        path={ROUTES.ADMIN_ROUTE_SETTINGS}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <SettingsAdmins />
+          </ProtectedPage>
+        }
+      />
       <Route
         path={ROUTES.IMPROVERISHED_ROUTE}
         element={
-          <SecurePage
-            isAuth={user.isAuth && user.role === UserRole.USER}
-            redirect={ROUTES.LOGIN_ROUTE}
-          >
-              <Navigate to={ROUTES.IMPROVERISHED_ROUTE_CLAIM} />
-          </SecurePage>
+          <ProtectedPage role={UserRole.ADMIN}>
+            <Navigate to={ROUTES.IMPROVERISHED_ROUTE_CLAIM} />
+          </ProtectedPage>
         }
       />
 
-        <Route
-            path={ROUTES.IMPROVERISHED_ROUTE_CLAIM}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <ClaimUser />
-                </SecurePage>
-            }
-        />
+      <Route
+        path={ROUTES.IMPROVERISHED_ROUTE_CLAIM}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <ClaimUser />
+          </ProtectedPage>
+        }
+      />
 
-        <Route
-            path={ROUTES.IMPROVERISHED_ROUTE_EDIT}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <EditUser />
-                </SecurePage>
-            }
-        />
+      <Route
+        path={ROUTES.IMPROVERISHED_ROUTE_EDIT}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <EditUser />
+          </ProtectedPage>
+        }
+      />
 
-        <Route
-            path={ROUTES.IMPROVERISHED_ROUTE_STORY}
-            element={
-                <SecurePage
-                    isAuth={user.isAuth && user.role === UserRole.ADMIN}
-                    redirect={ROUTES.LOGIN_ROUTE}
-                >
-                    <StoryUser />
-                </SecurePage>
-            }
-        />
+      <Route
+        path={ROUTES.IMPROVERISHED_ROUTE_STORY}
+        element={
+          <ProtectedPage role={UserRole.ADMIN}>
+            <StoryUser />
+          </ProtectedPage>
+        }
+      />
 
       <Route path={ROUTES.VOLONTEER_ROUTE} element={<VolonteerPage />} />
       <Route path={ROUTES.LOGIN_ROUTE} element={<Auth />} />
